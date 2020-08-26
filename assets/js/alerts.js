d6dcd3a07c75be92e8164d44d19aed36d7ce0fb5
Mariano Lopez <mariano@skywalker.local>
2020-08-26 19:39:59 -0300

    seccion 2

---
+++ assets/js/alerts.js
@@ -0,0 +1,15 @@
+// console.log(miNombre);
+
+// alert('hola mundo');
+
+/* let nombre = prompt('ingresa un valor', 'si nombre');
+console.log(nombre); */
+
+// promp puede devolver una cadena vacia y con cancel devuelve un null
+
+
+const seleccion = confirm('estas seguro de borrar??'); //el confirm devuelve true o false
+
+console.log(seleccion);
+
+
---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+// famoso hola mundo
+/* 
+console.log('Hola Mundo');
+document.write('escribo en el html');
+ */
+
+//  Variables
+/* 
+let a = 10; // si la uso antes de declararla ne tira error
+var b = 10;
+const c = 10; // nunca cambia
+
+ */
+
+// alert('hola desde app.js');
+
+
+let a = 10, 
+    b = 10, 
+    c = 'Hola ', 
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+/* 
+console.log(x);
+console.error(x);
+console.warn(x);
+console.info(x);
+
+console.log({a});
+console.log({b});
+console.log({c});
+console.log({d});
+console.log({x});
+
+
+console.table({a, b, c, d, x});
+ */
+c = 'Hola de nuevo';
+
+//var outerWidth = 1000000; //no es buena práctica hacer esto y usar el va
+//var outerHeight = 600; //toca las variables de window
+
+//no tocan las variables del objeto global window
+let outerWidth = 100;
+const outerHeight = 1000;
+
+
+
+
+
